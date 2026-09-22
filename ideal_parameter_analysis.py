be8466ed71527843c42e8625de79d363c3e823a6
Ritizz26 <ritizshrestha321@gmail.com>
2026-09-22 10:22:49 +0545

    Ideal parameter analysis by varying photoelectric current

---
+++ ideal_parameter_analysis.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+k=1.38e-23
+T=300
+q=1.602e-19
+Io=1e-10
+n=1
+Iph=np.array([0.030, 0.09, 0.15])  # Photocurrent values for different light intensities
+V=np.linspace(0,0.6,1000)
+for Iph in Iph:
+    I_ideal= Iph-Io*(np.exp(q*V/(k*T))-1)
+    Isc=I_ideal[0]
+    print("Iph=",Iph)
+    print("Isc=",Isc)
+    Voc_ind=np.argmin(np.abs(I_ideal))
+    Voc=V[Voc_ind]
+    print("Voc=",Voc)
+    P=I_ideal*V
+    Pmax=np.max(P)
+    print("Maximum Power:",Pmax)
+    ind=np.argmax(P)
+    Vmp=V[ind]
+    Imp=I_ideal[ind]
+    print("Imp:",Imp)
+    print("Vmp:",Vmp)
+    FF=Pmax/(Voc*Isc)
+    print("Fill factor:",FF)
+    plt.plot(V,I_ideal,label='Ideal I-V Curve for Iph={}'.format(Iph))
+plt.xlabel('Voltage(X)')
+plt.ylabel('Current(Y)')
+plt.grid()
+plt.legend()
+plt.title("I-V Curve of Solar Cell")
+plt.show()
