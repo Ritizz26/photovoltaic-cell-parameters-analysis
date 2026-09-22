aace572e6805ec81e95ea5ec0f7d4a4ae0c03037
Ritizz26 <ritizshrestha321@gmail.com>
2026-09-22 17:39:00 +0545

    Shunt resistance analysis by varying it and analyzing distinct IV curves

---
+++ series_resistance_analysis.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import brentq
+k=1.38e-23
+T=300
+q=1.602e-19
+Io=1e-10
+n=1
+Rs=np.array([0.5,1.5,2.5,3.5])  # Series resistance values
+Rsh=1000  # Shunt resistance value
+V=np.linspace(0,0.6,1000)
+
+Iph=0.030  # Photocurrent value
+def solar_equation(I_real,V,R):
+    return I_real-Iph+Io*(np.exp(q*(V+I_real*R)/(n*k*T))-1)+(V+I_real*R)/Rsh
+for R in Rs:
+    I_real=[]
+    for v in V:
+     i=brentq(solar_equation,-2,2,args=(v,R))
+     I_real.append(i)
+    I_real=np.array(I_real)
+    print("Rs=",R)
+    Isc=I_real[0]
+    print("Isc=",Isc)
+    Voc_ind=np.argmin(np.abs(I_real))
+    Voc=V[Voc_ind]
+    print("Voc=",Voc)
+    P=I_real*V
+    Pmax=np.max(P)
+    print("Maximum Power:",Pmax)
+    ind=np.argmax(P)
+    Vmp=V[ind]
+    Imp=I_real[ind]
+    print("Imp:",Imp)
+    print("Vmp:",Vmp)
+    FF=Pmax/(Voc*Isc)
+    print("Fill factor:",FF)
+    plt.plot(V,I_real,label='Real I-V Curve for Rs={}'.format(R))
+plt.xlabel('Voltage(X)')
+plt.ylabel('Current(Y)')
+plt.grid()
+plt.legend()
+plt.title("I-V Curve of Solar Cell")
+plt.show()
